40ddb3aeaaa7c7c6b12d2d38ce859d5876ab4bf9
oda251 <pgwyaueu@gmail.com>
2024-10-28 23:09:14 +0900

    update

--- 典型90/006_small_sub/main.cpp
+++ 典型90/006_small_sub/main.cpp
@@ -1,25 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> calc_map(string S)
-{
-	int N = S.size();
-	vector<vector<int>> dest(N, vector<int>(26, -1));
+int N, K;
+string S;
 
-	for (int i=N-1; i<=0; i--)
-	{
-		
+vector<vector<int>> calc_map(void) {
+	vector<vector<int>> dest(N + 1, vector<int>(26, -1));
+	for (int i=N-1; i>=0; i--) {
+		for (int j=0; j<26; j++) {
+			if (S[i] == 'a'+j)
+				dest[i][j] = i;
+			else
+				dest[i][j] = dest[i+1][j];
+		}
 	}
-
-
+	return dest;
 }
 
 int	main()
 {
-	int	N, K;
-	string S;
-	string sub;
-	char ini;
 	cin >> N >> K >> S;
+	auto dest = calc_map();
+	string sub;
+	sub.resize(K);
 
+	int cur = 0;
+	for (int i=0; i<K; i++) {
+		for (int j=0; j<26; j++) {
+			if (dest[cur][j] != -1 && N - dest[cur][j] >= K - i) {
+				sub[i] = 'a' + j;
+				cur = dest[cur][j] + 1;
+				break;
+			}
+		}
+	}
+	cout << sub << endl;
+	return 0;
 }
